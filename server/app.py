8249824fd894e701225e978228d8def131e67329
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-06-30 21:11:52 +0300

    fix app routes to use password setter and secure login auth method

--- server/app.py
+++ server/app.py
@@ -1,43 +1,68 @@
-#!/usr/bin/env python3
+from flask import Flask, request, session, jsonify
+from flask_restful import Api, Resource
+from models import db, User
+from config import app, db, api, bcrypt
 
-from flask import request, session
-from flask_restful import Resource
+# Allow credentials for session cookies
+from flask_cors import CORS
+CORS(app, supports_credentials=True)
 
-from config import app, db, api
-from models import User
-
-class ClearSession(Resource):
-
-    def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
-        return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
 
-class CheckSession(Resource):
-    pass
+        if not username or not password:
+            return {"error": "Username and password required"}, 400
+
+        # Use setter to avoid hybrid property error
+        new_user = User(username=username)
+        new_user.password_hash = password
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+        return new_user.to_dict(), 201
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {"error": "Invalid username or password"}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
-api.add_resource(Signup, '/signup', endpoint='signup')
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
+
+
+# Register resources
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
